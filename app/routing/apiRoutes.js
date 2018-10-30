ce793e45a2c3dad1b8b916fd392594f5d3c06593
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-10-30 10:42:19 -0400

    First commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+// Load Data
+// =================================================================
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+  // API Routes
+  // =================================================================
+  app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+      
+  });
+};
