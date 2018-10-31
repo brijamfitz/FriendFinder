d7c856f950dfe62c1097c5d3b0278d68f8f301e7
Brian Fitzgerald <brianjfitzgerald@gmail.com>
2018-10-30 22:46:52 -0400

    finished routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,11 +5,13 @@ var friends = require("../data/friends");
 module.exports = function(app) {
   // API Routes
   // =================================================================
+  // Display JSON data when client requests this path
   app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
-
+  // Posts client data at this path and handles all logic
   app.post("/api/friends", function(req, res) {
-      
+    friends.push(req.body);
+    res.json(true);
   });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,20 @@
-// Dependencies
+// Dependencies/Requires
 // =================================================================
 var path = require("path");
 
 // HTML Routes
 // =================================================================
 module.exports = function(app) {
+  // Home page
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-
+  // Survey page
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-
-//   app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/home.html"));
-//   });
+  // Send user to homepage if invalid path is requested
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
